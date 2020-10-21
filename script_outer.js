c8fb9f01479a53f46a4dd81f4eafec5220c69f24
Luis Rojas <luisrohu91@gmail.com>
2020-10-21 09:06:44 -0600

    add fucntion that returns the id of each planet

--- script_outer.js
+++ script_outer.js
@@ -1,21 +1,46 @@
 document.body.style.zoom = "25%";
 
+const API_KEY = 'TW2hym5fprpzfSEc15t1hI9zu59tLvI8tbCG22Zy'
+const API_URL_SOLAR_DATA = 'https://api.le-systeme-solaire.net/rest/bodies/'
+const API_NASA =`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
+
+// get data from API
+const getSolar = async() => {
+    try{
+        const response = await axios.get(API_URL_SOLAR_DATA)
+        // console.log(response.data)
+        // console.log(getPlanet(response))
+        getPlanetData(response)
+    } catch(error) {
+        console.log(error)
+    }
+}
+
+// matches the name of the planet with the body from 
+// the api
+const getPlanetData = (data) => {
+    const planets = data.data.bodies
+    for(let i=0; i < planets.length; i++) {
+        if(planets[i].isPlanet === true) {
+            console.log('works')
+        }
+    }
+}
+
+// gets planet selected name
+const getPlanet = (e) => {
+        return e.target.id
+}
+
+// queries
 const jupiter = document.querySelector('#jupiter')
 const saturn = document.querySelector('#saturn')
 const uranus = document.querySelector('#uranus')
 const neptune = document.querySelector('#neptune')
 
 
-
-jupiter.addEventListener('click', () => {
-    console.log('works: jupiter')     
-})
-saturn.addEventListener('click', () => {
-    console.log('works: saturn')     
-})
-uranus.addEventListener('click', () => {
-    console.log('works: uranus')     
-})
-neptune.addEventListener('click', () => {
-    console.log('works: neptune')     
-})
+// Event Listener
+jupiter.addEventListener('click', getPlanet)
+saturn.addEventListener('click', getPlanet)
+uranus.addEventListener('click', getPlanet)
+neptune.addEventListener('click', getPlanet)
