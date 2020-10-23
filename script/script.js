e463d1c469d130fa9192fab06e7f9c6eab0bc078
Luis Rojas <luisrohu91@gmail.com>
2020-10-22 19:12:51 -0600

    add function to crete divs and place them at random locations

--- script/script.js
+++ script/script.js
@@ -2,12 +2,20 @@ document.body.style.zoom = "25%"
 
 let body = document.querySelector('body')
 
-const star = document.createElement('div')
-star.className = 'stars'
-const effect = document.createElement('div')
-effect.className = 'effects'
-
-
-
-body.appendChild(star)
-body.appendChild(effect)
+// will create stars
+const createStars = () => {
+    for(let i=0; i < 100; i++) {
+        const star = document.createElement('div')
+        star.className = 'stars'
+        positionStar(star)
+        body.appendChild(star)
+    }
+}
+// will position the elements at random location
+const positionStar = (star) => {
+    let x = Math.floor(Math.random() * 100)
+    let y = Math.floor(Math.random() * 100)
+    star.style.left = x + '%'
+    star.style.top = y + '%'
+}
+createStars()
